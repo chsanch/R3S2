80c23e9da94465919bca291d398c3fd6de46845e
Christian <csanchez@unplug.org.ve>
2010-04-08 01:52:37 -0430

    Agregadas las opciones para ver el detalle del inscrito

--- lib/R3S2/Controller/Inscritos.pm
+++ lib/R3S2/Controller/Inscritos.pm
@@ -58,6 +58,33 @@ sub agrega :Chained('/sede') :PathPart('agrega') :Args(0) :FormConfig {
 }
 
 #Administracion
+
+sub lista :Chained('/admin') :PathPart('inscritos/lista') :Args(1) {
+    my ( $self, $c, $sede_id) = @_;
+    
+    if ($c->check_user_roles('coordinador')) { $sede_id = $c->user->sedes->first->id; }   
+        
+    $c->stash->{sede} = $c->model('DB::Sede')->find($sede_id);
+    $c->stash->{template} = 'inscritos/listasede.tt2';
+    $c->detach;
+    
+}
+
+sub inscritos :Chained('/admin') :PathPart('inscritos') :Args(0) {
+    my ( $self, $c ) = @_;
+    if ($c->check_user_roles('admin')) {
+        $c->stash->{sedes} = [$c->model('DB::Sede')->all];
+        $c->stash->{template} = 'inscritos/lista.tt2';
+        $c->detach;
+    }
+    if($c->check_user_roles('coordinador')){
+        my $sede_id = $c->user->sedes->first->id;
+        $c->response->redirect($c->uri_for('/admin/inscritos/lista', $sede_id));
+    }
+    
+}
+
+
 sub base :Chained('/admin') :PathPart('inscrito') :CaptureArgs(0) {
         my ($self, $c) = @_;
     
@@ -91,34 +118,6 @@ sub objeto :Chained('base') :PathPart('id') :CaptureArgs(1) {
 }
 
 
-
-sub inscritos :Chained('/admin') :PathPart('inscritos') :Args(0) {
-    my ( $self, $c ) = @_;
-    if ($c->check_user_roles('admin')) {
-        $c->stash->{sedes} = [$c->model('DB::Sede')->all];
-        $c->stash->{template} = 'inscritos/lista.tt2';
-        $c->detach;
-    }
-    if($c->check_user_roles('coordinador')){
-        my $sede_id = $c->user->sedes->first->id;
-        $c->response->redirect($c->uri_for('/admin/inscritos/lista', $sede_id));
-    }
-    
-}
-
-
-sub lista :Chained('/admin') :PathPart('inscritos/lista') :Args(1) {
-    my ( $self, $c, $sede_id) = @_;
-    
-    if ($c->check_user_roles('coordinador')) { $sede_id = $c->user->sedes->first->id; }   
-        
-    $c->stash->{sede} = $c->model('DB::Sede')->find($sede_id);
-    $c->stash->{template} = 'inscritos/listasede.tt2';
-    $c->detach;
-    
-}
-
-
 sub eliminar :Chained('objeto') :PathPart('eliminar') :Args(0) {
     my ($self, $c) = @_;
 
@@ -135,6 +134,13 @@ sub eliminar :Chained('objeto') :PathPart('eliminar') :Args(0) {
         $c->response->redirect($c->uri_for('/admin/inscritos/lista', $sede_id));
 }
 
+#detalle del inscrito
+sub detalle :Chained('objeto') :PathPart('detalle') :Args(0) {
+    my ( $self, $c ) = @_;
+    
+    $c->stash->{inscrito} = $c->stash->{objeto};
+    $c->stash->{template} = 'inscritos/detalle.tt2';
+}
 
 
 =head2 envia_not
